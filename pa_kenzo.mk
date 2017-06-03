a408af678fbdea3705e2fe68a31e3240812629e3
neonkat <neonkatxda@gmail.com>
2017-06-03 16:38:00 +0530

    paranoidsfnasjknfknesklne

--- pa_kenzo.mk
+++ pa_kenzo.mk
@@ -17,14 +17,20 @@
 
 $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Set bootanimation to 1080p display.
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Inherit from our common CAF device tree.
+include device/qcom/common/common.mk
+
+# Inherit the main configuration from our vendor.
+include vendor/pa/main.mk
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := lineage_kenzo
+PRODUCT_NAME := pa_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
